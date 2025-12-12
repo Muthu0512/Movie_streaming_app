25240219afa88ff089874a99a42f74d11a3c5118
Muthu0512 <masmuthu99@gmail.com>
2025-12-12 15:50:29 +0530

    login page logo modified

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -19,7 +19,7 @@ const LoginPage = () => {
      
     <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
     <Link to={"/"}>
-      <img src="/assets/netflix-logo.png" alt="logo" className="w-52"/>
+      <img src="/netflix-logo.png" alt="logo" className="w-52"/>
     </Link>
     </header>
      <div className="flex flex-col justify-center items-center mt-20 mx-3 ">
